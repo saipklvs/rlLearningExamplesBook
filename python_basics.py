70d0c92575ffbf1be11ad8df9a115e962d8498ba
vlottala <pavanlottala94@gmail.com>
2023-02-11 19:45:41 -0600

    changes the req

---
+++ python_basics.py
@@ -0,0 +1,18 @@
+#%%
+val_1 = list(range(20))
+print(val_1[:-1])
+# %%
+print(val_1[5:7])
+# %%
+import numpy as np
+# %%
+rank_1 = np.array([1, 2, 3])
+print(rank_1.shape)
+# %%
+b = np.array([[1, 2, 3], [4, 6, 9]])
+print(b.shape)
+# %%
+b[0], b[1]
+# %%
+b[-1]
+# %%
